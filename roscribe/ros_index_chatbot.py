4e7517d7923e738298397c794e6d5faf7235b041
RoboCoachian <85717407+RoboCoachian@users.noreply.github.com>
2023-11-21 14:26:11 -0800

    Update ros_index_chatbot.py

--- roscribe/ros_index_chatbot.py
+++ roscribe/ros_index_chatbot.py
@@ -11,27 +11,32 @@ from langchain.chat_models import ChatOpenAI
 import roscribe.ui as ui
 
 
-db_name = "ros_index_db_humble_2023_10_31"
-vectorstore = Chroma(persist_directory="./../ROS_index_database/" + db_name, embedding_function=OpenAIEmbeddings())
-retriever = vectorstore.as_retriever(search_type="mmr", search_kwargs={'k': 8})
+def main():
+    db_name = "ros_index_db_humble_2023_10_31"
+    vectorstore = Chroma(persist_directory="./../ROS_index_database/" + db_name, embedding_function=OpenAIEmbeddings())
+    retriever = vectorstore.as_retriever(search_type="mmr", search_kwargs={'k': 8})
+    
+    # Tool Declaration
+    tool = create_retriever_tool(
+        retriever,
+        "search_ROS_repositories",
+        "Searches and returns documents regarding the ROS repositories."
+    )
+    tools = [tool]
+    
+    # Agent Construction
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo-16k", temperature=0)
+    agent_executor = create_conversational_retrieval_agent(llm, tools, max_token_limit=6000, verbose=False)
+    
+    print(ui.WELCOME_MSG_RAG)
+    
+    while True:
+        print('\n\n')
+        human_input = input()
+        result = agent_executor({"input": human_input})
+        print('\n\n')
+        print(result["output"])
 
-# Tool Declaration
-tool = create_retriever_tool(
-    retriever,
-    "search_ROS_repositories",
-    "Searches and returns documents regarding the ROS repositories."
-)
-tools = [tool]
 
-# Agent Construction
-llm = ChatOpenAI(model_name="gpt-3.5-turbo-16k", temperature=0)
-agent_executor = create_conversational_retrieval_agent(llm, tools, max_token_limit=6000, verbose=False)
-
-print(ui.WELCOME_MSG_RAG)
-
-while True:
-    print('\n\n')
-    human_input = input()
-    result = agent_executor({"input": human_input})
-    print('\n\n')
-    print(result["output"])
+if __name__ == "__main__":
+    main()
